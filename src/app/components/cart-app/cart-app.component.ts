fc6f45a71d8df2cce7235cae09d2447e3c2b6a0e
juan Manuel <jmgv87191@gmail.com>
2024-04-29 12:56:24 -0700

    agregar

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -27,7 +27,25 @@ export class CartAppComponent implements OnInit {
   }
 
   onAddCart( product:Product ){
-    this.items = [...this.items, { product, quantity: 1 } ];
+    const hasItem = this.items.find( item =>{
+      return item.product.id === product.id
+    } )
+
+    if (hasItem) {
+      this.items = this.items.map( item => {
+        if( item.product.id === product.id ){
+          return {
+            ...item,
+            quantity: item.quantity +1
+          }
+        }
+        return item;
+      })
+    }else{
+      this.items = [...this.items, { product:{...product}, quantity: 1 } ];
+    }
+
+
   }
 
 
